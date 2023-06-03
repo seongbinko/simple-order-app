62f57719ad8ea02f2e8fad541ea16213af385e5d
seongbinko <wnrhd1082@gmail.com>
2023-06-03 15:19:35 +0900

    edit: h2 yml로 변경

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+insert into categories (name, created_at, modified_at) values('안주류', now(), now());
+insert into categories (name, created_at, modified_at) values('주류/음료', now(), now());
+
+
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('아이스황도',5000 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00193/1596181127item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('아이스크림',3500 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00195/1596181133item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('파인애플',8900 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_ISMJ006/M00420/1591436129item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('통삽겹살',10000 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GRD002/C00177/1601103514item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('파품닭',18000 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00226/1596179998item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('오돌뼈',13000 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00173/1596180072item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('엄마손주먹밥',4000 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00180/1596180192item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('케이준치킨',12000 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR005/M00238/1605797733item_700X700.png');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('오뎅탕',9900 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00175/1596180096item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('라면',3000 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00179/1596180154item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('계란찜',6000 , now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TOD_GRD_001/C00091/1601366775item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('치즈볼',4500, now(), now(),1, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00185/1596181071item_700X700_toJPEGBot.jpg');
+
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('진로',4500 , now(), now(),2, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00169/1596179931item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('처음처럼',4500 , now(), now(),2, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00198/1596180438item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('참이슬',4500 , now(), now(),2, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00201/1596180510item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('콜라',2000 , now(), now(),2, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00208/1596180769item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('사이다',2000 , now(), now(),2, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/M00209/1596180733item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('웰치스',2500 , now(), now(),2, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR003/C00043/1600420716item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('핫식스',2000 , now(), now(),2, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR005/C00124/1602354144item_700X700_toJPEGBot.jpg');
+insert into items (name , price, created_at, modified_at, category_id, img_url) values ('갈아만든배',2000 , now(), now(),2, 'https://s3.ap-northeast-2.amazonaws.com/images.orderhae.com/TPAY_GDR005/C00125/1602431371item_700X700_toJPEGBot.jpg');
